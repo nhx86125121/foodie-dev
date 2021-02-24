d090c44ae443f96b5a452d01938528f3fb5036be
nhx <6125121q@163.com>
2021-02-24 17:30:38 +0800

    用户注册

--- foodie-dev-api/src/main/java/com/imooc/Application.java
+++ foodie-dev-api/src/main/java/com/imooc/Application.java
@@ -2,13 +2,17 @@ package com.imooc;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import tk.mybatis.spring.annotation.MapperScan;
 
 /**
  * @author:Nhx
  * @description
  * @date:2021-02-23
  */
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.imooc","org.n3r.idworker"})
+// 扫描 mybatis 通用 mapper 所在的包
+@MapperScan(basePackages = "com.imooc.mapper")
 public class Application {
 
     public static void main(String[] args) {
---
+++ foodie-dev-service/src/main/java/com/imooc/service/UserService.java
@@ -0,0 +1,27 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Users;
+import com.imooc.pojo.bo.UserBO;
+
+/**
+ * @author:Nhx
+ * @description
+ * @date:2021-02-24
+ */
+public interface UserService {
+
+    /**
+     * 判断用户名是否存在
+     */
+    boolean queryUsernameIsExist(String username);
+
+    /**
+     * 判断用户名是否存在
+     */
+    Users createUser(UserBO userBO);
+
+    /**
+     * 检索用户名和密码是否匹配，用于登录
+     */
+    Users queryUserForLogin(String username, String password);
+}

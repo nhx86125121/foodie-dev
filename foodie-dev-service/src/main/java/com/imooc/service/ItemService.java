3f64fbd98e1e94d064d7ee5ac62baf981a32e4c9
nhx <6125121q@163.com>
2021-03-02 15:17:22 +0800

    商品详情

---
+++ foodie-dev-service/src/main/java/com/imooc/service/ItemService.java
@@ -0,0 +1,45 @@
+package com.imooc.service;
+
+import com.imooc.pojo.Items;
+import com.imooc.pojo.ItemsImg;
+import com.imooc.pojo.ItemsParam;
+import com.imooc.pojo.ItemsSpec;
+
+import java.util.List;
+
+/**
+ * @author:Nhx
+ * @description
+ * @date:2021-03-02
+ */
+public interface ItemService {
+
+    /**
+     * 根据商品ID查询详情
+     * @param itemId
+     * @return
+     */
+    public Items queryItemById(String itemId);
+
+    /**
+     * 根据商品id查询商品图片列表
+     * @param itemId
+     * @return
+     */
+    public List<ItemsImg> queryItemImgList(String itemId);
+
+    /**
+     * 根据商品id查询商品规格
+     * @param itemId
+     * @return
+     */
+    public List<ItemsSpec> queryItemSpecList(String itemId);
+
+    /**
+     * 根据商品id查询商品参数
+     * @param itemId
+     * @return
+     */
+    public ItemsParam queryItemParam(String itemId);
+
+}
